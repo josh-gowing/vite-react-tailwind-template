f5c7172df3a3ea63cdecb40a4ca990b12e3ec121
Josh <josh@Mac.lan>
2025-04-08 11:09:54 +0100

    Initial Vite + React + Tailwind Template

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <div className="text-center text-2xl font-bold text-blue-600">
+        🚀 Vite + React + TailwindCSS Template Ready!
+      </div>
+    </>
+  )
+}
+
+export default App
